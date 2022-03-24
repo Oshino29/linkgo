d3d812032a929ca4edd1d122f0b9778856b3c923
OshinoLi <leaaachy@gmail.com>
2022-03-24 17:09:43 +0800

    js fetch
    fetch failed
    
    merge conflicts

---
+++ frontend/main.js
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+// fetch('https://linkgo.ccllssd.com/items')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+fetch('linkgo:8080/items')
+.then(response => response.json())
+.then(data => console.log(data));
+=======
+fetch('https://linkgo.ccllssd.com/items')
+  .then(response => response.json())
+  .then(data => console.log(data));
+>>>>>>> main
