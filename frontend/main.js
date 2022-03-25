ff8ff2936acee41cb504ecf07d35aa5b7d9be300
oshino <oshino@ccllssd.com>
2022-03-25 18:25:51 +0800

    js working with html5 <template> tag

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,34 @@
-fetch('http://localhost:8080/items')
-  .then(response => response.json())
-  .then(data => console.log(data));
+// fetch('http://localhost:8080/items/frontend')
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+
+// fetch('http://localhost:8080/items', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//         title: 'codepen.io trending',
+//         url: 'https://codepen.io/trending',
+//         tags: ['dev:frontend', 'community']
+//     })
+// }).then(res => {
+//     return res.json()
+// })
+// .then(data => console.log(data))
+// .catch(error => console.log('ERROR'))
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('js executed after document loaded')
+    let temp = document.getElementById('cardTemplate')
+    let content = temp.content.cloneNode(true)
+    console.log(content)
+    for (let index = 0; index < 30; index++) {
+        console.log(`add ${index}th element`)
+
+        content = temp.content.cloneNode(true)
+        document.getElementById('cardContainer').appendChild(content)
+    }
+})
