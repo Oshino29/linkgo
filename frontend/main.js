3d19067e1e3fcbccb224b19c1eab017740f4ad1c
oshino <oshino@ccllssd.com>
2022-03-26 01:25:59 +0800

    add new item with input field and button

--- frontend/main.js
+++ frontend/main.js
@@ -36,22 +36,51 @@
 //     }
 // })
 
+// main functions start
+document.addEventListener('DOMContentLoaded', () => showAllItems())
+
+// const testItem = {
+//     title: "ウラアカ批評空間",
+//     url: "https://uraakaspace.com",
+//     tags: ["community selfhosted"]
+// }
+
+// postItem(testItem).then(() => showAllItems())
+// main functions end
+
+// function definition starts here
+
+// show all items
 async function getAllItems() {
-    const response =  await fetch('http://localhost:8080/items').then(response => response.json())  
-    return response
+    const items = fetch('http://localhost:8080/items').then(response => response.json())  
+    return items
 }
 
+async function showAllItems() {
+    getAllItems().then(items => showItems(items))
+}
+function showItems(items) {
+    let card = makeCard('testAllTag')
+    items.forEach(item => {
+        let itemHTML = makeItem(item)
+        addItemToCard(itemHTML, card)
+    })
+    addCardToDom(card)
+}
+
+// make a card with tag name
 function makeCard(tag) {
     const templateCard = document.getElementById('cardTemplate').content
     let card = document.importNode(templateCard, true)
     card.querySelector('.card-title').textContent = tag
     return card
 }
-
+// append a card to DOM
 function addCardToDom(card) {
     document.getElementById('cardContainer').appendChild(card)
 }
 
+// make an item with item object
 function makeItem(item) {
     const templateItem = document.getElementById('itemTemplate').content
     let itemHTML = document.importNode(templateItem, true)
@@ -59,16 +88,34 @@ function makeItem(item) {
     itemHTML.querySelector('a').href = item.url
     return itemHTML
 }
-
+// add an item to a card made bt makeCard()
 function addItemToCard(itemHTML, card) {
     card.querySelector('.item-container').appendChild(itemHTML)
 }
 
-getAllItems().then(items => {
-    let card = makeCard('testAllTag')
-    items.forEach(item => {
-        let itemHTML = makeItem(item)
-        addItemToCard(itemHTML, card)
-    })
-    addCardToDom(card)
-})
+
+function addItembyInput() {
+    const item = readInput()
+    postItem(item)
+    showAllItems()
+}
+
+function readInput() {
+    const item = {
+        title: document.getElementById('titleInput').value,
+        url: document.getElementById('urlInput').value
+    }
+    return item
+}
+// post an item with an item object
+async function postItem(item) {
+    const request = new Request('http://localhost:8080/items', {
+        method: 'POST',
+        body: JSON.stringify(item),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const response = await fetch(request);
+    //   return response.json()
+}
