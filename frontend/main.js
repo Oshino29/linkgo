a0fe6f6e55015a125e1182389ddf40ef661db64e
oshino <oshino@ccllssd.com>
2022-03-26 00:31:46 +0800

    display all fetched items in one test card

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,4 @@
-// fetch('http://localhost:8080/items/frontend')
-//   .then(response => response.json())
-//   .then(data => console.log(data))
+
 
 // fetch('http://localhost:8080/items', {
 //     method: 'POST',
@@ -20,15 +18,57 @@
 
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('js executed after document loaded')
-    let temp = document.getElementById('cardTemplate')
-    let content = temp.content.cloneNode(true)
-    console.log(content)
-    for (let index = 0; index < 30; index++) {
-        console.log(`add ${index}th element`)
+// document.addEventListener('DOMContentLoaded', () => {
+//     console.log('js executed after document loaded')
+//     let templateCard = document.getElementById('cardTemplate').content
+//     let templateItem = document.getElementById('itemTemplate').content
+//     let card = document.importNode(templateCard, true)
+//     let item = document.importNode(templateItem, true)
 
-        content = temp.content.cloneNode(true)
-        document.getElementById('cardContainer').appendChild(content)
-    }
+//     console.log(card)
+//     for (let index = 0; index < 30; index++) {
+//         console.log(`add ${index}th element`)
+
+        
+
+//         card.content.getElementsByClassName('item-container').appendChild(item)
+//         document.getElementById('cardContainer').appendChild(card)
+//     }
+// })
+
+async function getAllItems() {
+    const response =  await fetch('http://localhost:8080/items').then(response => response.json())  
+    return response
+}
+
+function makeCard(tag) {
+    const templateCard = document.getElementById('cardTemplate').content
+    let card = document.importNode(templateCard, true)
+    card.querySelector('.card-title').textContent = tag
+    return card
+}
+
+function addCardToDom(card) {
+    document.getElementById('cardContainer').appendChild(card)
+}
+
+function makeItem(item) {
+    const templateItem = document.getElementById('itemTemplate').content
+    let itemHTML = document.importNode(templateItem, true)
+    itemHTML.querySelector('.item-title').textContent = item.title
+    itemHTML.querySelector('a').href = item.url
+    return itemHTML
+}
+
+function addItemToCard(itemHTML, card) {
+    card.querySelector('.item-container').appendChild(itemHTML)
+}
+
+getAllItems().then(items => {
+    let card = makeCard('testAllTag')
+    items.forEach(item => {
+        let itemHTML = makeItem(item)
+        addItemToCard(itemHTML, card)
+    })
+    addCardToDom(card)
 })
